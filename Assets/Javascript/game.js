d9548d982b7a9cbdb3d84e9dc10c63e71ad9ee47
woodtop <cwood89@mac.com>
2018-10-18 04:13:43 -0400

    added javascript code

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,2 +1,18 @@
-<script>
-</script>
+// global variables
+wins = 0;
+number = randNum();
+losses = 0;
+$("#random-number").text(number);
+$("#win").text(wins);
+$("#loss").text(losses);
+// $("#score).innerHtml();
+// generate random number
+function randNum() { 
+    return Math.floor(Math.random() * (101) + 19);
+};
+// generate random values of crystals
+// add crystal values together
+// determine if crystal value is equal to random number
+// if value is equal then win
+//if value is over then lose
+// restart game
