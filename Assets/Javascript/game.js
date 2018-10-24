048f9fe9ce584e9cdf6551057854fdb9581f4a6c
woodtop <cwood89@mac.com>
2018-10-24 14:21:58 -0400

    attempt to debug win/loss function

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -5,7 +5,6 @@ var score = 0;
 var random = randNum();
 var crystal;
 
-console.log(random);
 // DOM element selection
 $("#random-number").text(random);
 $("#win").text("Wins: " + wins);
@@ -17,33 +16,42 @@ $("#blue").val(Math.floor(Math.random() * (12) + 1));
 $("#green").val(Math.floor(Math.random() * (12) + 1));
 $("#purple").val(Math.floor(Math.random() * (12) + 1));
 $("#red").val(Math.floor(Math.random() * (12) + 1));
+$("#score").text(score);
 score = 0;
 random = randNum();
-crystal = 0;;
-};start();
+crystal = 0;
+};
 
 // generate random number
 function randNum() { 
     return Math.floor(Math.random() * (101) + 19);
 };
 
+start();
+
 // add crystal values together
 $("button").on("click" , function() {
     crystal = $(this).val();
     crystal = parseInt(crystal);
     score += crystal;
-    console.log(this);
-    console.log(score);
-    $("#score").text(score)
+    $("#score").text(score);
+    checkWin();
 });
 
 // determine if crystal value is equal to random number
-if (score===random){
+function checkWin() {
 // if value is equal then win
-wins++;
-} else if (score > random) { 
+if (score === random) {
+    wins++;
+    alert("You Win!");
+    start();
+    }
 //if value is over then 
-losses++;
+else if (score > random) { 
+    losses++;
+    alert("Sorry Buddy");
+    start();
+    };
+    $("#win").text("Wins: " + wins);
+    $("#loss").text("Losses: " + losses);
 };
-// restart game
-start();
