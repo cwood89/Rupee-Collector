54d8f8f071c030e80904e43508c1fcc5ea26a302
woodtop <cwood89@mac.com>
2018-10-23 23:55:39 -0400

    homework due

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,18 +1,49 @@
 // global variables
-wins = 0;
-number = randNum();
-losses = 0;
-$("#random-number").text(number);
-$("#win").text(wins);
-$("#loss").text(losses);
-// $("#score).innerHtml();
+var wins = 0;
+var losses = 0;
+var score = 0;
+var random = randNum();
+var crystal;
+
+console.log(random);
+// DOM element selection
+$("#random-number").text(random);
+$("#win").text("Wins: " + wins);
+$("#loss").text("Losses: " + losses);
+
+function start() {
+// generate random values of crystals
+$("#blue").val(Math.floor(Math.random() * (12) + 1));
+$("#green").val(Math.floor(Math.random() * (12) + 1));
+$("#purple").val(Math.floor(Math.random() * (12) + 1));
+$("#red").val(Math.floor(Math.random() * (12) + 1));
+score = 0;
+random = randNum();
+crystal = 0;;
+};start();
+
 // generate random number
 function randNum() { 
     return Math.floor(Math.random() * (101) + 19);
 };
-// generate random values of crystals
+
 // add crystal values together
+$("button").on("click" , function() {
+    crystal = $(this).val();
+    crystal = parseInt(crystal);
+    score += crystal;
+    console.log(this);
+    console.log(score);
+    $("#score").text(score)
+});
+
 // determine if crystal value is equal to random number
+if (score===random){
 // if value is equal then win
-//if value is over then lose
+wins++;
+} else if (score > random) { 
+//if value is over then 
+losses++;
+};
 // restart game
+start();
